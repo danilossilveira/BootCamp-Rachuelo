f0554f54c849ce8686717ac37e38440331bfc738
Danilo <danilosantossilveira.com@gmail.com>
2025-08-14 11:50:29 -0300

    Add files via upload

--- BootCampRiachuelo/src/Main.java
+++ BootCampRiachuelo/src/Main.java
@@ -2,11 +2,83 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Olá, informe seu nome");
-        var name = scanner.next();
-        System.out.println("Olá, informe sua idade");
-        var age = scanner.nextInt();
-        System.out.printf("Olá s% e sua idade é %s \n", name, age);
+
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+//int value = 50;
+//System.out.println(++value);
+//  System.out.println(value);
+
+
+
+
+
+//Scanner scanner = new Scanner(System.in);
+//System.out.println("Informe o primeiro número");
+//int value1 = scanner.nextInt();
+//System.out.printf("a raiz quadrada de %s é s%\n", value1, Math.pow(value1, 2));
+//System.out.println("A raiz de " + value1 + " é " + Math.pow(value1, 2));
+
+
+
+
+
+//       int value = 5;
+//       value +=12;
+//        System.out.println(value);
+
+
+
+
+
+
+
+        /*System.out.println("Informe o segundo número");
+        float value2 = scanner.nextFloat();
+
+        System.out.println(value1 + " + " + value2 + " = " + (value1 + value2));
+        System.out.printf("%s - %s = %s\n", value1, value2, value1 - value2);
+        System.out.println(value1 + " - " + value2 + " = " + (value1 - value2));*/
+
+
+
+
+
+
+/*System.out.printf("true && true = %s \n", true && true);
+        System.out.printf("false && false = %s \n", false && false);
+        System.out.printf("true && false = %s \n", true && false);
+        System.out.printf("false && true = %s \n", false && false);
+        System.out.println("==================================");
+         System.out.printf("true || true = %s \n", true || true);
+        System.out.printf("false || false = %s \n", false || false);
+        System.out.printf("true || false = %s \n", true || false);
+        System.out.printf("false || true = %s \n", false || true);
+        System.out.println("==================================");
+        System.out.printf("!true = %s\n", !true);
+        System.out.printf("!true = %s\n", !false);*/
+
+
+
+
+
+/*Scanner scanner = new Scanner(System.in);
+        System.out.println("Quantos anos você tem?");
+        int age = scanner.nextInt();
+        System.out.println("Você é emancipado?");
+        boolean isEmancipated = scanner.nextBoolean();
+        boolean canDrive = age >= 18 || (isEmancipated && age >=16);
+        System.out.println("Você pode dirigir? " + canDrive);*/
+
